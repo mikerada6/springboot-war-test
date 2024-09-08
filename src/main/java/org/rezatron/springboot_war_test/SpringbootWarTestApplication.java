acf0b95160b8dadeb59134b8060d89f3df6fbdb6
Radaszkiewicz, Michael <Michael_Radaszkiewicz@comcast.com>
2024-09-08 12:06:31 -0400

    Initial commit

---
+++ src/main/java/org/rezatron/springboot_war_test/SpringbootWarTestApplication.java
@@ -0,0 +1,13 @@
+package org.rezatron.springboot_war_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWarTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rezatron/springboot_war_test/SpringbootWarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rezatron.springboot_war_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
