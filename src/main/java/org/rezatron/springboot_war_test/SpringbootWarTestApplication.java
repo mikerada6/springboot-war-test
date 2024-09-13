ddb9ada8ef3f7967542fd95af25041759e657776
Radaszkiewicz, Michael <Michael_Radaszkiewicz@comcast.com>
2024-09-13 12:21:07 -0400

    Remove ignored files from repo

---
+++ src/main/java/org/rezatron/springboot_war_test/SpringbootWarTestApplication.java
@@ -0,0 +1,22 @@
+package org.rezatron.springboot_war_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
+
+@SpringBootApplication
+public class SpringbootWarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWarTestApplication.class, args);
+	}
+
+	@Bean
+MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+  return registry -> registry.config().commonTags("application", "springboot_war_test");
+}
+
+}
